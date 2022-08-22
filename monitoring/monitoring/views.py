d92f88fa16e14287b2202d03d9553257b86516d8
Vladimir Vasilev <vasilevvladv@yandex.ru>
2022-08-22 13:53:25 +0400

    Add templates index.html

--- monitoring/monitoring/views.py
+++ monitoring/monitoring/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    context = {}
+    return render(request, 'monitoring/index.html', context)
