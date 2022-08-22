6de51263f270800b6dc255a361f20d07f3804f68
Vladimir Vasilev <vasilevvladv@yandex.ru>
2022-08-23 01:05:53 +0400

    Add monitoringisp views function and templates

--- monitoring/monitoring_isp/views.py
+++ monitoring/monitoring_isp/views.py
@@ -1,9 +1,22 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .models import MonitoringIsp
 
 
 def monitorigisp(request):
-    return HttpResponse('Список мороженого')
+    page_obj = MonitoringIsp.objects.none()
+    listuch =[]
+    a = list(MonitoringIsp.objects.values('nomeruch').distinct())
+    for x in a:
+        listuch.append(*x.values())
+    for x in listuch:
+        findobj=MonitoringIsp.objects.filter(nomeruch=x).latest('monitoringdata') 
+        page_obj |= MonitoringIsp.objects.filter(pk = findobj.pk)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'monitoring_isp/monitorigisp.html', context)
 
 def monitorigisp_detail(request, pk):
     return HttpResponse(f'Судебный участок {pk}') 
