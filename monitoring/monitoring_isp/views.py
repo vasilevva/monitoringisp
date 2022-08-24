50d1d632c4bffe6e77474a80815ed79aa5b0a62c
Vladimir Vasilev <vasilevvladv@mail.ru>
2022-08-24 14:53:33 +0300

    Add reult moinitoring database

--- monitoring/monitoring_isp/views.py
+++ monitoring/monitoring_isp/views.py
@@ -1,3 +1,4 @@
+import fdb
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
@@ -22,15 +23,29 @@ def monitorigisp(request):
     }
     if request.method == 'POST' and form.is_valid():
         objuch = form.cleaned_data['nomeruch']
-        #nomeruch = getattr(objuch, 'numer')
-        #obj_dbamirs = DbAmirs.objects.filter(nomeruch=nomeruch)
         obj_dbamirs = get_object_or_404(DbAmirs, nomeruch=objuch)
-        server = 
-        path =
-        port =
-        host = 
-        print(lll)
-        print('!!!!!!!!!!!!!!!!!!!!!')
+        host = getattr(obj_dbamirs.server, 'ip')
+        path = obj_dbamirs.pathtodb
+        port = obj_dbamirs.port
+        user = obj_dbamirs.login
+        password = obj_dbamirs.password
+        sql_dialect = obj_dbamirs.sql_dialect
+        charset = obj_dbamirs.charset
+        print(host, path, port, user, password, sql_dialect, charset)
+        con = fdb.connect(host=host,
+                  port=port,
+                  database=path,
+                  user=user,
+                  password=password,
+                  sql_dialect=sql_dialect,
+                  charset=charset)
+        cur = con.cursor()
+        cur.execute('''SELECT COUNT("OID") FROM "ExternalSystemLogRecord" WHERE "LogMessage" LIKE 'Создана квитанция о подтверждении%';''')
+        receiveddoc = cur.fetchall()[0][0]
+
+        MonitoringIsp.objects.create(receiveddoc = receiveddoc, opencase = 0,
+                                    nomeruch =  objuch)
+                
     return render(request, 'monitoring_isp/monitorigisp.html', context)
 
 def monitorigisp_detail(request, pk):
